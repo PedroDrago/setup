a07b31d59969624190f0660e1b6b6c80b1bd5127
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2023-04-04 03:45:38 -0300

    a bunch of stuff

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install -y ansible curl git software-properties-common
 sudo ansible-pull -U https://github.com/PedroDrago/ansible
-cd ansible
