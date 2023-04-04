fcb6c6cc1eb5afb8d6a4937422e5e5d420002da6
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2023-04-04 04:46:53 -0300

    whatever

--- build_machine.sh
+++ build_machine.sh
@@ -1,2 +1,2 @@
 #ansible-playbook -t dotfiles ansible_all.yml --ask-become-pass --ask-vault-pass #tag dotfiles inside ansible_all
-ansible-playbook ansible_all.yml 
+ansible-playbook --ask-vault-password local.yml
--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
-sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt install -y ansible curl git software-properties-common
-sudo ansible-pull -U https://github.com/PedroDrago/ansible
+# sudo apt-add-repository --yes --update ppa:ansible/ansible
+# sudo apt install -y ansible curl git software-properties-common 
+# git clone https://github.com/PedroDrago/ansible && cd ansible
+# bash build_machine.sh
+# sudo ansible-pull -U https://github.com/PedroDrago/ansible
+
+
+sudo apt update && sudo apt upgrade -y && sudo apt install -y ansible curl git software-properties-common && git clone https://github.com/PedroDrago/ansible && cd ansible && ansible-playbook --ask-vault-password local.yml
