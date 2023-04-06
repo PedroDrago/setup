900c67b06cff24a90047ac271938c4bfbdaef952
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2023-04-06 09:03:50 -0300

    asddsa

--- correct_rust_version.sh
+++ correct_rust_version.sh
@@ -1,5 +1,5 @@
 #get line that's written in .zshrc defining path with old version
-zshrc_old=$(cat .zshrc | grep installs/rust)
+zshrc_old=$(cat $HOME/.zshrc | grep installs/rust)
 #get new version that was installed with adsf
 latest_version=$(asdf list rust)
 #create the string to be ready to be placed in .zshrc, the :2 is to deleting an * and a space from the version string
