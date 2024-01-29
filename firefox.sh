b536e5e24b668fb592781a479a76fbe2801a2ff8
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-01-29 10:36:12 -0300

    w

--- firefox.sh
+++ firefox.sh
@@ -1,1 +1,9 @@
-echo -e 'Package: *\nPin: release o=LP-PPA-mozillateam\nPin-Priority: 1001\n\nPackage: firefox\nPin: version 1:1snap1-0ubuntu2\nPin-Priority: -1' | tee /etc/apt/preferences.d/mozilla-firefox
+echo '
+Package: *
+Pin: release o=LP-PPA-mozillateam
+Pin-Priority: 1001
+
+Package: firefox
+Pin: version 1:1snap1-0ubuntu2
+Pin-Priority: -1
+' | sudo tee /etc/apt/preferences.d/mozilla-firefox
