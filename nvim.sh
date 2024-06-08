81addd37e0ef8d3aed5eeb0011f0f7ff6b045c44
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-06-07 22:58:58 -0300

    docker

--- nvim.sh
+++ nvim.sh
@@ -1,6 +1,6 @@
-VERSION=release-0.10
 git clone https://github.com/neovim/neovim.git $HOME/neovim
 cd $HOME/neovim
-git checkout -b $VERSION
+git checkout release-0.10
 make CMAKE_BUILD_TYPE=Release
 sudo make install
+rm -rf $HOME/neovim
