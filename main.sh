c47c698d39b17093e0b75b12a5d9a46df6195c35
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-06-08 11:54:02 -0300

    zsh test

--- main.sh
+++ main.sh
@@ -6,7 +6,7 @@ bash $HOME/setup/vscode.deb
 bash $HOME/setup/docker.sh
 bash $HOME/setup/home.sh
 bash $HOME/setup/zsh.sh
-zsh -c $HOME/setup/omz.sh
+bash $HOME/setup/omz.sh
 bash $HOME/setup/dotfiles.sh
 bash $HOME/setup/asdf.sh
 bash $HOME/setup/rust_tools.sh
