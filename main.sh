7a34f5caedd31c79de3e9973bb0091cd070f6f21
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-06-07 23:07:03 -0300

    idk

--- main.sh
+++ main.sh
@@ -7,4 +7,7 @@ bash $HOME/setup/rust_tools.sh
 bash $HOME/setup/nvim.sh
 bash $HOME/setup/chrome.deb
 bash $HOME/setup/vscode.deb
+bash $HOME/setup/docker.sh
+bash $HOME/setup/home.sh
+bash $HOME/setup/mount.sh
 sudo reboot -f
