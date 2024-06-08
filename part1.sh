2a0b66ac48a4d5981ee35a3468b93126712edde5
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-06-08 12:40:58 -0300

    teste

--- part1.sh
+++ part1.sh
@@ -6,3 +6,8 @@ bash $HOME/setup/vscode.deb
 bash $HOME/setup/docker.sh
 bash $HOME/setup/home.sh
 bash $HOME/setup/omz.sh
+bash $HOME/setup/omz_extensions.sh
+bash $HOME/setup/dotfiles.sh
+zsh $HOME/setup/asdf.sh
+zsh $HOME/setup/langs.sh
+zsh $HOME/setup/rust_tools.sh
