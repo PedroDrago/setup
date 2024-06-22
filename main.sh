79ee59b35ddfb53f3985d749b2a170744b5f5a8e
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-06-22 03:23:23 -0300

    remove graphical stuff

--- main.sh
+++ main.sh
@@ -1,8 +1,6 @@
 bash $HOME/setup/installs.sh
 bash $HOME/setup/ssh.sh
 bash $HOME/setup/nvim.sh
-bash $HOME/setup/chrome.deb
-bash $HOME/setup/vscode.deb
 bash $HOME/setup/docker.sh
 bash $HOME/setup/home.sh
 bash $HOME/setup/omz.sh
