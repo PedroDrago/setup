291b39b7271b3a08a78a366af4e935a9926d1731
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-06-28 15:07:09 -0300

    omz refactor

--- omz.sh
+++ omz.sh
@@ -1,2 +1,5 @@
 echo -e "\033[32mRunning omz.sh\033[0m"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting
