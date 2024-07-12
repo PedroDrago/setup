0f55bb7afe079dbc31d4c7427304f1df5157db70
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-07-11 23:15:55 -0300

    add firefox dev isntallation

--- main.sh
+++ main.sh
@@ -2,6 +2,7 @@ bash $HOME/setup/installs.sh
 bash $HOME/setup/ssh.sh
 bash $HOME/setup/nvim.sh
 bash $HOME/setup/chrome.deb
+bash $HOME/setup/firefox-dev.sh
 bash $HOME/setup/vscode.deb
 bash $HOME/setup/docker.sh
 bash $HOME/setup/home.sh
