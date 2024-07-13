80532138cc6f8ff5ec67e9d57712e72cb02382de
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-07-13 02:10:18 -0300

    change ssh moving to copying each file

--- ssh.sh
+++ ssh.sh
@@ -1,3 +1,4 @@
 echo -e "\033[32mRunning ssh.sh\033[0m"
 ansible-vault decrypt $HOME/setup/.ssh/id_rsa
-mv $HOME/setup/.ssh/* $HOME/.ssh/
+cp $HOME/setup/.ssh/id_rsa $HOME/.ssh/
+cp $HOME/setup/.ssh/id_rsa.pub $HOME/.ssh/
