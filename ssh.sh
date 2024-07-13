401d18a40b7070f3a551d9f9d93fe24703495ce1
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-07-13 06:08:53 -0300

    comment

--- ssh.sh
+++ ssh.sh
@@ -2,3 +2,4 @@ echo -e "\033[32mRunning ssh.sh\033[0m"
 ansible-vault decrypt $HOME/setup/.ssh/id_rsa
 cp $HOME/setup/.ssh/id_rsa $HOME/.ssh/
 cp $HOME/setup/.ssh/id_rsa.pub $HOME/.ssh/
+# For some reason this did not work with `mv $HOME/setup/.ssh/*`, I hard coded both keys but the ideal would be a wildcard, for the case I have multiple ssh keys in `.ssh` directory
