27fa66f21d5822372b49663260fb75cc3f78a67a
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-07-23 08:58:47 -0300

    pip update

--- langs.sh
+++ langs.sh
@@ -1,7 +1,12 @@
 echo -e "\033[32mRunning langs.sh\033[0m"
 source $HOME/.zshrc
 
+# Rust
 mise use --global rust
+# Go
 mise use --global go
+# Python
 mise use --global python
+pip install --upgrade pip
+# Node
 mise use --global node
