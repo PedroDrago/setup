6b839c55085a034fc5931a047cc94e9019596e0d
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-10-21 14:07:35 -0300

    update

---
+++ alacritty.sh
@@ -0,0 +1,3 @@
+cargo install alacritty
+sudo rm -f /usr/bin/alacritty
+sudo ln -s .local/share/mise/installs/rust/latest/bin/alacritty /usr/bin/alacritty
--- packages.sh
+++ packages.sh
@@ -4,15 +4,3 @@ source $HOME/.zshrc
 # Rust
 #  FIX: for some reason this is using the wrong rustc version when using the script? after reboot just executing cargo install eza/alacritty works fine
 cargo install eza
-cargo install alacritty
-sudo rm -f /usr/bin/alacritty
-sudo ln -s .local/share/mise/installs/rust/latest/bin/alacritty /usr/bin/alacritty
-
-# # Go
-# go install -tags 'postgres' github.com/golang-migrate/migrate/v4/cmd/migrate@latest
-# go install github.com/air-verse/air@latest
-
-# Python
-# pip install --upgrade pip
-# pip install pipx
-# pipx install uv --user
