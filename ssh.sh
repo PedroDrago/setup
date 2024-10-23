e7fe7e255fb26ee55ed4a756f1235c22d118e258
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-10-23 16:10:58 -0300

    update

--- ssh.sh
+++ ssh.sh
@@ -3,4 +3,4 @@ ansible-vault decrypt $HOME/setup/.ssh/id_rsa
 mkdir -p $HOME/.ssh
 # cp $HOME/setup/.ssh/id_rsa $HOME/.ssh/
 # cp $HOME/setup/.ssh/id_rsa.pub $HOME/.ssh/
-cp $HOME/setup/.ssh/* $HOME/.ssh/
+cp $HOME/setup/.ssh/* $HOME/.ssh/ #if this fails next time, investigate. The above worked.
