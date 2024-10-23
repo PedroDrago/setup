2776f298da1c2a0e5e2745e4d6986d0ae74c3375
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-10-23 17:34:28 -0300

    change dotfiles HTTP URL for an SSH URL

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,5 +1,5 @@
 echo -e "\033[32mRunning dotfiles.sh\033[0m"
 
-git clone --recurse-submodules https://github.com/PedroDrago/.dotfiles.git $HOME/.dotfiles
+git clone --recurse-submodules git@github.com:PedroDrago/.dotfiles.git $HOME/.dotfiles
 cd $HOME/.dotfiles
 ./install
