e93d54612366743af311aa80101df56221e0fa55
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-10-23 17:44:37 -0300

    warning message

--- fonts.sh
+++ fonts.sh
@@ -1,7 +1,7 @@
 echo -e "\033[32mRunning fonts.sh\033[0m"
 
 
-echo -e "\033[33m[WARNING]: current downloading JetBrainsMono v3.2.1 as a hardcoded URL; a newer version may exist\033[0m"
+echo -e "\033[33m[WARNING]: current downloading v3.2.1 as a hardcoded URL; a newer version may exist\033[0m"
 mkdir -p $HOME/.local/share/fonts
 #https://www.nerdfonts.com/font-downloads
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/JetBrainsMono.zip
