4b1f6dd3d50e4c9dfb274450bf81d566920eec21
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-11-03 05:22:47 -0300

    add man to installs

--- installs.sh
+++ installs.sh
@@ -1,7 +1,7 @@
 echo -e "\033[32mRunning common_installs.sh\033[0m"
 
 # May need to add rustc? removed to try to fix cargo problem in this script
-sudo apt install git curl wget vim tree ansible fzf bat tmux arandr ripgrep fd-find feh stow lsof xclip zsh ncdu lldb valgrind htop unzip software-properties-common build-essential ca-certificates gnupg wget libpq-dev libssl-dev fontconfig imagemagick libmagickwand-dev ncurses-term exuberant-ctags make zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev libffi-dev liblzma-dev autoconf bison libyaml-dev libreadline6-dev libgmp-dev libncurses5-dev libgdbm6 libgdbm-dev libdb-dev uuid-dev libcurl4-openssl-dev icu-devtools libicu-dev numactl libfuse2 coreutils ninja-build gettext cmake -y
+sudo apt install git curl wget vim tree ansible fzf bat tmux arandr ripgrep man fd-find feh stow lsof xclip zsh ncdu lldb valgrind htop unzip software-properties-common build-essential ca-certificates gnupg wget libpq-dev libssl-dev fontconfig imagemagick libmagickwand-dev ncurses-term exuberant-ctags make zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev libffi-dev liblzma-dev autoconf bison libyaml-dev libreadline6-dev libgmp-dev libncurses5-dev libgdbm6 libgdbm-dev libdb-dev uuid-dev libcurl4-openssl-dev icu-devtools libicu-dev numactl libfuse2 coreutils ninja-build gettext cmake -y
 
 kernel=$(uname -r)
 
