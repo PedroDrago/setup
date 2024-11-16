6188f5f43f188350e27800f1e8ee16591138c983
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-11-16 12:46:44 -0300

    add beta mount script

--- installs.sh
+++ installs.sh
@@ -1,6 +1,6 @@
 echo -e "\033[32mRunning installs.sh\033[0m"
 
-sudo apt install git curl wget vim tree ansible fzf bat tmux arandr ripgrep man rsync tealdeer fd-find feh stow lsof xclip zsh ncdu lldb valgrind htop unzip software-properties-common build-essential ca-certificates gnupg wget libpq-dev libssl-dev fontconfig imagemagick libmagickwand-dev ncurses-term exuberant-ctags make zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev libffi-dev liblzma-dev autoconf bison libyaml-dev libreadline6-dev libgmp-dev libncurses5-dev libgdbm6 libgdbm-dev libdb-dev uuid-dev libcurl4-openssl-dev icu-devtools libicu-dev numactl libfuse2 coreutils ninja-build gettext cmake -y
+sudo apt install git curl wget vim tree ansible fzf bat tmux arandr ripgrep man rsync tealdeer fd-find feh stow lsof xclip zsh ncdu lldb valgrind htop unzip software-properties-common build-essential ca-certificates meson gnupg wget libpq-dev libssl-dev fontconfig imagemagick libmagickwand-dev ncurses-term exuberant-ctags make zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev libffi-dev liblzma-dev autoconf bison libyaml-dev libreadline6-dev libgmp-dev libncurses5-dev libgdbm6 libgdbm-dev libdb-dev uuid-dev libcurl4-openssl-dev icu-devtools libicu-dev numactl libfuse2 coreutils ninja-build gettext cmake -y
 
 DISTRO=$(cat /proc/version)
 IS_DEBIAN=$(echo "$DISTRO" | grep -i debian)
--- mount.sh
+++ mount.sh
@@ -1,1 +1,27 @@
 #automount shared drive
+
+exit 1 #This script is not prod ready
+LABEL="shared"
+MOUNT_POINT="/mnt/shared"
+FSTAB="/etc/fstab"
+
+if [ ! -d "$MOUNT_POINT" ]; then
+    sudo mkdir -p "$MOUNT_POINT"
+fi
+
+DEVICE=$(blkid -L "$LABEL")
+
+if [ -z "$DEVICE" ]; then
+    echo "No device found with label '$LABEL'."
+    exit 1
+fi
+
+sudo cp $FSTAB ${FSTAB}.backup.$(date +%Y%m%d%H%M%S)
+if ! grep -qs "$DEVICE" "$FSTAB" && ! grep -qs "LABEL=$LABEL" "$FSTAB"; then
+    echo "LABEL=$LABEL $MOUNT_POINT auto defaults 0 0" | sudo tee -a $FSTAB
+
+    echo "Entry added to $FSTAB. You can now mount it with 'sudo mount -a'."
+else
+    echo "The device is already in $FSTAB."
+fi
+sudo mount -a
