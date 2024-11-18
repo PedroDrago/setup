eb50db2f9c4d3f375543bc8d2bbe53b32ba16aff
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-11-18 15:47:46 -0300

    add mise eval

--- alacritty.sh
+++ alacritty.sh
@@ -1,5 +1,6 @@
 echo -e "\033[32mRunning alacritty.sh\033[0m"
 
-$HOME/.local/share/mise/installs/rust/latest/bin/cargo isntall alacritty
+eval $(/home/pdrago/.local/bin/mise activate bash)
+$HOME/.local/share/mise/installs/rust/latest/bin/cargo install alacritty
 sudo rm -f /usr/bin/alacritty
 sudo ln -s .local/share/mise/installs/rust/latest/bin/alacritty /usr/bin/alacritty
--- eza.sh
+++ eza.sh
@@ -1,3 +1,5 @@
 echo -e "\033[32mRunning eza.sh\033[0m"
 
-$HOME/.local/share/mise/installs/rust/latest/bin/cargo isntall eza
+
+eval $(/home/pdrago/.local/bin/mise activate bash)
+$HOME/.local/share/mise/installs/rust/latest/bin/cargo install eza
