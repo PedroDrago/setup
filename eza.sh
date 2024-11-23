b6d427b41641c4663e08a2349802711da08a12f4
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-11-23 06:56:04 -0300

    comment mise script evaluation

--- eza.sh
+++ eza.sh
@@ -1,5 +1,5 @@
 echo -e "\033[32mRunning eza.sh\033[0m"
 
 
-eval $(/home/pdrago/.local/bin/mise activate bash)
+# eval $(/home/pdrago/.local/bin/mise activate bash) # FIX: didnt work
 $HOME/.local/share/mise/installs/rust/latest/bin/cargo install eza
