2c10340942b958527be3ca30367b899fb1d781a9
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-12-28 18:54:32 -0300

    update

--- debian.sh
+++ debian.sh
@@ -3,6 +3,7 @@ bash $HOME/setup/i3.sh
 bash $HOME/setup/ssh.sh
 bash $HOME/setup/nvim.sh
 bash $HOME/setup/chrome.deb
+bash $HOME/setup/obsidian.sh
 bash $HOME/setup/firefox-dev.sh
 bash $HOME/setup/vscode.deb
 bash $HOME/setup/docker.sh
