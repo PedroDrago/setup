268f74cfe5582531228d2659518550361e68a8fc
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-12-28 19:08:59 -0300

    update mount

--- ghostty.sh
+++ ghostty.sh
@@ -1,5 +1,7 @@
 echo -e "\033[32mRunning ghostty.sh\033[0m"
 
+
+echo -e "\033[32mCheck if deb package already supported\033[0m"
 git clone https://github.com/ghostty-org/ghostty.git $HOME/ghostty
 cd $HOME/ghostty && zig build -Doptimize=ReleaseFast #how to build from "distance"?
 sudo mv $HOME/ghostty/zig-out/bin/ghostty /usr/bin/
