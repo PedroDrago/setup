9dc082c9aa6f8a50a9cdd23b6e3ed0b5ba89026f
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2025-02-07 21:05:21 -0300

    ssh revamp

--- ssh.sh
+++ ssh.sh
@@ -1,5 +1,12 @@
 echo -e "\033[32mRunning ssh.sh\033[0m"
 
-ansible-vault decrypt $HOME/setup/.ssh/id_rsa
-mkdir -p $HOME/.ssh
-cp $HOME/setup/.ssh/* $HOME/.ssh/
+SSH_DIR="$HOME/.ssh"
+FILES="$SSH_DIR/azure/id_rsa $SSH_DIR/github/id_rsa" #substitute for automation that finds all id_rsa files inside sshdir
+TARGET_DIR="$HOME/setup/.ssh"
+# Find all id_rsa files recursively and store them in a single string
+
+rm -rf $HOME/.ssh
+cp -r "$HOME/setup/.ssh" $HOME/.ssh
+RSA_FILES=$(find "$SSH_DIR" -type f -name "id_rsa" -print | tr '\n' ' ')
+
+ansible-vault decrypt $RSA_FILES
