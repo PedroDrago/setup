fa88ddfea7b0dd041ed9b32d006ff37aa0c3a143
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2025-05-17 20:25:15 -0300

    up

--- arch/arch.sh
+++ arch/arch.sh
@@ -1,5 +1,6 @@
 bash $HOME/setup/arch/installs.sh
 bash $HOME/setup/arch/yay.sh
+bash $HOME/setup/arch/yay_installs.sh
 bash $HOME/setup/arch/ssh.sh
 bash $HOME/setup/arch/nvim.sh
 bash $HOME/setup/arch/chrome.deb
@@ -8,9 +9,8 @@ bash $HOME/setup/arch/vscode.sh
 bash $HOME/setup/arch/docker.sh
 bash $HOME/setup/arch/omz.sh
 bash $HOME/setup/arch/dotfiles.sh
-bash $HOME/setup/arch/fonts.sh
 bash $HOME/setup/arch/mise.sh
 bash $HOME/setup/arch/langs.sh
-bash $HOME/setup/arch/cargo_tools.sh
+bash $HOME/setup/arch/cargo.sh
 chsh -s $(/usr/bin/zsh)
 sudo reboot -f
