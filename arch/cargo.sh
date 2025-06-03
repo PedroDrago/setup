98b24fd9d264ac1169526b5842c964fc2a116109
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2025-06-02 22:41:07 -0300

    up

--- arch/cargo.sh
+++ arch/cargo.sh
@@ -1,12 +1,1 @@
 echo -e "\033[32mRunning cargo_tools.sh\033[0m"
-
-
-# TODO: fix
-# eval $(/home/pdrago/.local/bin/mise activate bash)
-cargo install eza 
-cargo install alacritty 
-sudo rm -f /usr/bin/alacritty
-# sudo ln -s .local/share/mise/installs/rust/latest/bin/alacritty /usr/bin/alacritty
-sudo ln -s .local/share/mise/installs/rust/latest/alacritty /usr/bin/alacritty
-cargo install ripgrep 
-cargo install fd-find
--- arch/nvim.sh
+++ arch/nvim.sh
@@ -2,7 +2,7 @@ echo -e "\033[32mRunning nvim.sh\033[0m"
 
 git clone https://github.com/neovim/neovim.git $HOME/neovim
 cd $HOME/neovim
-git checkout releae-0.10 # NOTE: update when stable
+git checkout release-0.10 # NOTE: update when stable
 make CMAKE_BUILD_TYPE=Release
 sudo make install
 rm -rf $HOME/neovim
