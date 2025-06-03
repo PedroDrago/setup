4a0a8ca17e0cca023919506945f1cf88c929e538
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2025-06-03 01:39:25 -0300

    update add network manager to installs

--- arch/installs.sh
+++ arch/installs.sh
@@ -10,7 +10,7 @@ fi
 sudo pacman -S --needed --noconfirm base-devel git curl unzip vim tree ansible fzf man rsync feh stow xclip zsh ca-certificates meson gnupg wget fontconfig imagemagick make llvm autoconf bison numactl coreutils gettext cmake net-tools wl-clipboard bat tmux tealdeer lsof ncdu lldb valgrind htop clang fuse2 powertop ripgrep eza fd openssh
 
 if [[ "$KERNEL" != *"WSL"* ]]; then
-    pacman -Syu --needed --noconfirm alacritty waybar pavucontrol swaync ttf-jetbrains-mono-nerd rofi-wayland arandr 
+    pacman -Syu --needed --noconfirm alacritty waybar pavucontrol swaync ttf-jetbrains-mono-nerd rofi-wayland arandr networkmanager 
     yay -S google-chrome visual-studio-code-bin 
 fi
 
