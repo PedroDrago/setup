b17b7ee66223724199a5241474b6390a9ccac436
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2025-10-25 18:33:10 -0300

    up lock

---
+++ tmp/luarocks.sh
@@ -0,0 +1,8 @@
+mise use --global lua
+# luarocks
+wget https://luarocks.github.io/luarocks/releases/luarocks-3.12.2.tar.gz
+tar -xvf luarocks-3.12.2.tar.gz
+cd luarocks-3.12.2.tar.gz
+./configure
+make
+sudo make install
