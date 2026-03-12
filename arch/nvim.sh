d3466d8e656f3789ee6150b22aeef448b3277445
pedro drago <jupurity6@gmail.com>
2026-03-12 20:58:54 -0300

    up

--- arch/nvim.sh
+++ arch/nvim.sh
@@ -4,7 +4,7 @@ set -e
 echo -e "\033[31mNEED TO UPDATE NEOVIM CONFIG TO 0.11\033[0m"
 git clone https://github.com/neovim/neovim.git $HOME/neovim
 cd $HOME/neovim
-git checkout release-0.10 # NOTE: update when stable
+git checkout release-0.11 # NOTE: update when stable
 make CMAKE_BUILD_TYPE=Release
 sudo make install
 rm -rf $HOME/neovim
--- debian/nvim.sh
+++ debian/nvim.sh
@@ -3,7 +3,7 @@ set -e
 
 git clone https://github.com/neovim/neovim.git $HOME/neovim
 cd $HOME/neovim
-git checkout release-0.10
+git checkout release-0.11
 make CMAKE_BUILD_TYPE=Release
 sudo make install
 rm -rf $HOME/neovim
